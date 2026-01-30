edce1a2727a0dc2def45808addb40bd2f77009b8
SLAB43 <SLAB43@KM101-03-M>
2026-01-30 09:53:42 +0100

    30/01

--- api1_deambrogio/MainWindow.xaml.cs
+++ api1_deambrogio/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -11,9 +13,8 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Newtonsoft.Json;
+
+using static System.Net.Mime.MediaTypeNames;
 
 namespace api1_deambrogio
 {
@@ -25,79 +26,177 @@ namespace api1_deambrogio
         public MainWindow()
         {
             InitializeComponent();
-            _: GetWeather("Paris");
+            _ = GetWeather("Paris");
         }
 
         public async Task<string> GetWeather(string city)
         {
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://www.prevision-meteo.ch/services/json/Annecy");
-
+            HttpResponseMessage response = await client.GetAsync($"https://www.prevision-meteo.ch/services/json/Annecy");
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
 
-                Root root = JsonConvert.DeserializeObject<Root>(content);
+                var root = JsonConvert.DeserializeObject<Root>(content);
 
-               // Today
+                // Today
 
-               var today = root.fcst_day_0;
+                var today = root.fcst_day_0;
 
                 var temp = root.current_condition.tmp;
-                Température_actuelle.Text = temp.ToString() + " °C";
+                Température_actuelle.Text = temp.ToString() + "°C";
                 Vent.Text = root.current_condition.wnd_spd.ToString() + " km/h";
                 Température_max.Text = today.tmax.ToString() + " °C";
-                Température_min.Text =  today.tmin.ToString() + " °C";
-                Temps.Text = today.condition_key.ToString();
-                Condition.Text = root.current_condition.humidity.ToString() + " %";
+                Température_min.Text = today.tmin.ToString() + " °C";
+                Condition.Text = (root.current_condition?.humidity ?? 0).ToString() + " %";
+                Temps.Text = root.current_condition.condition ?? "";
 
-                double totalPrecipMm = 0;
-                if (today != null && today.hourly_data != null)
-                {
-                    var hourProps = today.hourly_data.GetType().GetProperties();
-                    foreach (var p in hourProps)
-                    {
-                        var hourObj = p.GetValue(today.hourly_data);
-                        if (hourObj == null) continue;
+                // Précipitations aujourd'hui
+                double totalPrecipMm = SumPrecipitation(today.hourly_data);
+                Precipitation.Text = totalPrecipMm.ToString("F2") + " mm";
 
-                        var apcpProp = hourObj.GetType().GetProperty("APCPsfc");
-                        if (apcpProp == null) continue;
-
-                        var val = apcpProp.GetValue(hourObj);
-                        if (val == null) continue;
-
-
-                        try
-                        {
-                            totalPrecipMm += Convert.ToDouble(val);
-                        }
-                        catch
-                        {
-                            // ignorer valeurs non convertibles
-                        }
-                    }
-                }
-                
-
-                // jour1
+                // jour1 (fcst_day_1)
                 var jour1 = root.fcst_day_1;
+                if (jour1 != null)
+                {
+                    Day.Text = jour1.day_long ?? "";
+                    Date.Text = jour1.date ?? "";
+                    Icon.Text = jour1.icon_big ?? jour1.icon ?? "";
+                    double ressenti1 = ComputeRessenti(jour1.hourly_data, jour1.tmax, jour1.tmin);
+                    Temp.Text = (jour1.condition ?? "");
+                    temp_max.Text = jour1.tmax.ToString() + "°";
+                    temp_min.Text = " / " + jour1.tmin.ToString() + "°";
+                    precipitation.Text = "💧 " + SumPrecipitation(jour1.hourly_data).ToString("F2") + " mm";
+                }
 
-                //Jour2
-                var jour2 = root.fcst_day_2.day_long;
+                // jour2 (fcst_day_2)
+                var jour2 = root.fcst_day_2;
+                if (jour2 != null)
+                {
+                    DAY.Text = jour2.day_long ?? "";
+                    DATE.Text = jour2.date ?? "";
+                    ICON.Text = jour2.icon_big ?? jour2.icon ?? "";
+                    double ressenti2 = ComputeRessenti(jour2.hourly_data, jour2.tmax, jour2.tmin);
+                    TEMP.Text = (jour2.condition ?? "");
+                    Temp_max.Text = jour2.tmax.ToString() + "°";
+                    Temp_min.Text = " / " + jour2.tmin.ToString() + "°";
+                    Précipitation.Text = "💧 " + SumPrecipitation(jour2.hourly_data).ToString("F2") + " mm";
+                }
 
+                // jour3 (fcst_day_3)
+                var jour3 = root.fcst_day_3;
+                if (jour3 != null)
+                {
+                    jour.Text = jour3.day_long ?? "";
+                    dates.Text = jour3.date ?? "";
+                    Icons.Text = jour3.icon_big ?? jour3.icon ?? "";
+                    double ressenti3 = ComputeRessenti(jour3.hourly_data, jour3.tmax, jour3.tmin);
+                    temperature.Text = (jour3.condition ?? "");
+                    TEMP_max.Text = jour3.tmax.ToString() + "°";
+                    TEMP_min.Text = " / " + jour3.tmin.ToString() + "°";
+                    Precip.Text = "💧 " + SumPrecipitation(jour3.hourly_data).ToString("F2") + " mm";
+                }
 
-                //Jour3
-                var jour3 = root.fcst_day_3.day_long;
+                // jour4 (fcst_day_4)
+                var jour4 = root.fcst_day_4;
+                if (jour4 != null)
+                {
+                    Jour.Text = jour4.day_long ?? "";
+                    date.Text = jour4.date ?? "";
+                    Images.Text = jour4.icon_big ?? jour4.icon ?? "";
+                    double ressenti4 = ComputeRessenti(jour4.hourly_data, jour4.tmax, jour4.tmin);
+                    Tempé.Text = (jour4.condition ?? "");
+                    tempé_max.Text = jour4.tmax.ToString() + "°";
+                    tempé_min.Text = " / " + jour4.tmin.ToString() + "°";
+                    précipitation.Text = "💧 " + SumPrecipitation(jour4.hourly_data).ToString("F2") + " mm";
+                }
 
-                //jour4
-                var jour4 = root.fcst_day_4.day_long;
-
+                // Mettre à jour la date/heure de mise à jour si disponible (ex : current_condition.hour)
+                DerniereMiseAJour.Text = root.current_condition?.hour ?? DerniereMiseAJour.Text;
             }
             else
             {
                 var tt = "error";
             }
             return null;
+        }
+
+        // Somme sécurisée des APCPsfc horaires (lecture via reflection, parse selon type)
+        private double SumPrecipitation(HourlyData hd)
+        {
+            if (hd == null) return 0;
+            double total = 0;
+            var props = hd.GetType().GetProperties();
+            foreach (var p in props)
+            {
+                var hourObj = p.GetValue(hd);
+                if (hourObj == null) continue;
+                var apcpProp = hourObj.GetType().GetProperty("APCPsfc");
+                if (apcpProp == null) continue;
+                var val = apcpProp.GetValue(hourObj);
+                if (val == null) continue;
+
+                double parsed = 0;
+                if (val is double dv) parsed = dv;
+                else if (val is float fv) parsed = fv;
+                else if (val is int iv) parsed = iv;
+                else if (val is long lv) parsed = lv;
+                else if (val is decimal decv) parsed = (double)decv;
+                else
+                {
+                    var s = val.ToString();
+                    double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out parsed);
+                }
+
+                total += parsed;
+            }
+            return total;
+        }
+
+        // Calcul simple du "ressenti" : moyenne des TMP2m horaires si existants sinon (tmax + tmin)/2
+        private double ComputeRessenti(HourlyData hd, int? tmax, int? tmin)
+        {
+            if (hd != null)
+            {
+                var props = hd.GetType().GetProperties();
+                double sum = 0;
+                int count = 0;
+                foreach (var p in props)
+                {
+                    var hourObj = p.GetValue(hd);
+                    if (hourObj == null) continue;
+                    var tmpProp = hourObj.GetType().GetProperty("TMP2m");
+                    if (tmpProp == null) continue;
+                    var val = tmpProp.GetValue(hourObj);
+                    if (val == null) continue;
+
+                    double parsed = 0;
+                    if (val is double dv) parsed = dv;
+                    else if (val is float fv) parsed = fv;
+                    else if (val is int iv) parsed = iv;
+                    else if (val is long lv) parsed = lv;
+                    else
+                    {
+                        double.TryParse(val.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out parsed);
+                    }
+
+                    sum += parsed;
+                    count++;
+                }
+
+                if (count > 0)
+                {
+                    return sum / count;
+                }
+            }
+
+            // fallback : moyenne tmax/tmin
+            if (tmax.HasValue && tmin.HasValue)
+            {
+                return (tmax.Value + tmin.Value) / 2.0;
+            }
+
+            return 0;
         }
     }
 
@@ -107,6 +206,7 @@ namespace api1_deambrogio
         public string ICON { get; set; }
         public string CONDITION { get; set; }
         public string CONDITION_KEY { get; set; }
+
         public int TMP2m { get; set; }
         public double DPT2m { get; set; }
         public object WNDCHILL2m { get; set; }
@@ -833,54 +933,29 @@ namespace api1_deambrogio
 
     public class HourlyData
     {
-
-       
         public _0H00 _0H00 { get; set; }
-   
         public _1H00 _1H00 { get; set; }
-
         public _2H00 _2H00 { get; set; }
-
         public _3H00 _3H00 { get; set; }
-
         public _4H00 _4H00 { get; set; }
-
         public _5H00 _5H00 { get; set; }
- 
         public _6H00 _6H00 { get; set; }
-
         public _7H00 _7H00 { get; set; }
-
         public _8H00 _8H00 { get; set; }
-
         public _9H00 _9H00 { get; set; }
-
         public _10H00 _10H00 { get; set; }
-  
         public _11H00 _11H00 { get; set; }
-
         public _12H00 _12H00 { get; set; }
-
         public _13H00 _13H00 { get; set; }
-
         public _14H00 _14H00 { get; set; }
-
         public _15H00 _15H00 { get; set; }
-
         public _16H00 _16H00 { get; set; }
- 
         public _17H00 _17H00 { get; set; }
-
         public _18H00 _18H00 { get; set; }
-
         public _19H00 _19H00 { get; set; }
-
         public _20H00 _20H00 { get; set; }
-
         public _21H00 _21H00 { get; set; }
-
         public _22H00 _22H00 { get; set; }
-
         public _23H00 _23H00 { get; set; }
     }
 
